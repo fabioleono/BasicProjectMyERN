73ec6ea993e00bb51838ed63d805f8a4df2b3d35
Fabioleon <fabioleono@gmail.com>
2020-09-23 16:39:36 -0500

    incluyendo algunos estilos css y scss

--- src/client/components/Organisms/navMenu.jsx
+++ src/client/components/Organisms/navMenu.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 const NavMenu = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/Users">
-            Usuarios
-          </NavLink>
-        </li>
-        <li></li>
-        <li>
-          <NavLink to="/Test">
-            Test
-          </NavLink>
-        </li>
-      </ul>
+    <div className="container">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" exact>
+                Home
+              </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Users">Usuarios</NavLink>
+          </li>
+          <li></li>
+          <li>
+            <NavLink to="/Test">Test</NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 export default NavMenu  
